03297d2caf65102d3a5cd079352f3490068530f9
Jederrr <ilhamderry111@gmail.com>
2023-05-25 08:57:58 +0700

    Latihan 6

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+        public function index()
+        {
+            echo "<h1>Perkenalkan</h1>";
+            echo"Nama saya Risky Ramadhani
+            Saya tinggal di daerah Cibubur
+            Olah raga yang saya sukai adalah Berenang";
+        }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Risky</li>
+ <li>Nama Belakang : Ramadhani</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Danapuri No.19 Harjamukti Cimanggis Depok</li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Depok</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Berenang</li>
+ <li>Catur</li>
+ </ul>
+</section>
